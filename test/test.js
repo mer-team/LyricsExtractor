f403a4394b4ee6781700d90dbc20405293577c5f
Canoso <jpcanoso@hotmail.com>
2021-05-31 15:38:27 +0100

    Updated test dir

--- test/test.js
+++ test/test.js
@@ -80,7 +80,7 @@ describe('Testing RabbitMQ', ()=>{
 
   it('Should split the music', function(done) {
     setTimeout(function(){
-      fs.access(`${GITHUB_WORKSPACE}/JiF3pbvR5G0/${file}`, fs.F_OK, (err) => {
+      fs.access(`${GITHUB_WORKSPACE}/test/JiF3pbvR5G0/${file}`, fs.F_OK, (err) => {
         if (err) {
           console.error(err)
           console.log("File not found!");
